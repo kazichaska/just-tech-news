96c3d1bd5a94ee5b9b7b6a1a19dfae3e94468b7c
Kazi Islam <kazichaska@gmail.com>
2022-02-21 22:44:04 -0600

    user can vote on post

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,14 +1,22 @@
 const router = require('express').Router();
+const req = require('express/lib/request');
 const res = require('express/lib/response');
 const { post } = require('.');
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection');
 
 // get a;; users
 router.get('/', (req, res) => {
     console.log('============================');
     Post.findAll({
         // Query configuration
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id', 
+            'post_url', 
+            'title', 
+            'created_at',
+            [sequelize.literal('(select count(*) from vote where post.id = vote.post_id)'), 'vote_count']
+        ],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -29,7 +37,13 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id', 
+            'post_url', 
+            'title', 
+            'created_at'
+            [sequelize.literal('(select count(*) from vote where post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
             {
                 model: User,
@@ -62,6 +76,17 @@ router.post('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+    // custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+        .then(updatedPostData => res.json(updatedPostData))
+        .catch(err =>  {
+            console.log(err);
+            res.status(400).json(err);
+        });
 });
 
 router.put('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -12,6 +12,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
+// IMPORTANT
+// Like earlier, once you turn on the server with 
+// sequelize.sync({ force: true }) and confirm the database tables were 
+// recreated, switch back to using { force: false } and restart the server 
+// one more time just to make sure the changes took hold and you don't 
+// accidentally remove data!
+//  `sequelize.sync({ force: true })` anytime there is update on 
+// the database side and turn it back to false when done
 sequelize.sync({ force: false }).then(() => {
         app.listen(PORT, () => console.log('Now listening!'));
 });
