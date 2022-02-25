3128b9d25d7540df116de08a0b668950d917aa6b
Kazi Islam <kazichaska@gmail.com>
2022-02-24 19:07:30 -0600

    comment model and route created and tested

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const req = require('express/lib/request');
 const res = require('express/lib/response');
 const { post } = require('.');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 // get a;; users
@@ -19,6 +19,14 @@ router.get('/', (req, res) => {
         ],
         order: [['created_at', 'DESC']],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -45,6 +53,14 @@ router.get('/:id', (req, res) => {
             [sequelize.literal('(select count(*) from vote where post.id = vote.post_id)'), 'vote_count']
         ],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
