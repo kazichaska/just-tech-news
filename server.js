b02f73740f65e75ca373881f73f45c1486399904
Kazi Islam <kazichaska@gmail.com>
2022-03-01 20:05:01 -0600

    optimize and cleanup

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
